8b769aabefd752df8ae0f330c847b7312db7993c
jindal-om <om.jindal@joshtechnologygroup.com>
2026-01-08 17:02:39 +0530

    [OJ_A3_BE_03] Fixed filter for query optimization

--- apps/movie/pagination.py
+++ apps/movie/pagination.py
@@ -6,7 +6,7 @@ class MoviePagination(CursorPagination):
     Pagination class for Movie list API.
     """
 
-    page_size = 1
+    page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 50
     ordering = "-release_date"
