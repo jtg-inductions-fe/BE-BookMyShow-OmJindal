9208018487b64e9422bb3c00f2db42c3639f28fd
jindal-om <om.jindal@joshtechnologygroup.com>
2026-01-29 18:35:49 +0530

    [OJ_A3_BE_10] Updated doc string, removed booking views and permissions, added constant for errors, and minor review fixes

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -1,30 +1,63 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from apps.user.models import User
+from django.contrib.auth import forms
+
+from apps.user import models as user_models
 
 
-class CustomUserCreationForm(UserCreationForm):
+class CustomUserCreationForm(forms.UserCreationForm):
     """
     Form used in Django admin to create new users.
 
-    Ensures password hashing and validation.
+    This form extends Django's built-in UserCreationForm to support
+    the custom User model.
+
+    Fields:
+        name (str): Full name of the user.
+        email (str): Unique email address of the user.
+        phone_number (str): Phone number of the user.
+        city (City): Reference to the city modal for user's city.
+        profile_picture (Image): Profile image uploaded by the user.
+        is_staff (bool): Determines if the user can access Django admin panel.
+        is_superuser (bool): Grants all permissions to the user.
     """
 
     class Meta:
-        model = User
-        fields = ("email", "name", "phone_number")
+        model = user_models.User
+        fields = (
+            "name",
+            "email",
+            "phone_number",
+            "city",
+            "profile_picture",
+            "is_staff",
+            "is_superuser",
+        )
 
 
-class CustomUserChangeForm(UserChangeForm):
+class CustomUserChangeForm(forms.UserChangeForm):
     """
     Form used in Django admin to update existing users.
+
+    This form extends Django's built-in UserChangeForm to support
+    updates to the custom User model.
+
+    Fields:
+        name (str): Full name of the user.
+        email (str): Unique email address of the user.
+        phone_number (str): Phone number of the user.
+        city (City): Reference to the city modal for user's city.
+        profile_picture (Image): Profile image uploaded by the user.
+        is_staff (bool): Determines if the user can access Django admin panel.
+        is_superuser (bool): Grants all permissions to the user.
+        is_active (bool): Controls whether the user account is active.
     """
 
     class Meta:
-        model = User
+        model = user_models.User
         fields = (
             "email",
             "name",
             "phone_number",
+            "city",
             "profile_picture",
             "is_active",
             "is_staff",
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,93 +1,86 @@
-from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
-from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import ListModelMixin, UpdateModelMixin
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework import status
+from rest_framework import generics as rest_generics
+from rest_framework import permissions as rest_permissions
 
-from apps.user.serializers import (
-    SignUpSerializer,
-    UserProfileSerializer,
-    ProfileUpdateSerializer,
-    BookingHistorySerializer,
-    BookingCancelSerializer,
-)
-from apps.user.permission import IsOwnerOrReadOnly
-from apps.slot.models import Booking
+from apps.user import serializers as user_serializers
 
 
-class SignupView(CreateAPIView):
+class SignupView(rest_generics.CreateAPIView):
     """
-    API view to create user's profile and return
-    JWT refresh and access tokens
-    """
+    API endpoint for registering new users and issuing authentication tokens.
 
-    serializer_class = SignUpSerializer
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        refresh = RefreshToken.for_user(user)
-        return Response(
-            {
-                "user": serializer.data,
-                "refresh": str(refresh),
-                "access": str(refresh.access_token),
-            },
-            status=status.HTTP_201_CREATED,
-        )
-
-
-class ProfileView(RetrieveUpdateAPIView):
-    """
-    API view to retrieve and update authenticated user's profile.
+    HTTP Method: POST
+        Request Body:
+                - name: User's full name.,
+                - email: User's email address.
+                - password: User's password.
+                - confirm_password: User's confirm password.
+        Response (201 Created):
+                - name: User's full name.,
+                - email: User's email address.
+                - access: "<jwt_access_token>",
+                - refresh: "<jwt_refresh_token>"
+        Error :
+            400 Bad Request:
+                - user with this email already exists.
+                - This password is too common.
+                - This password is too short. It must contain at least 8 characters.
+                - Passwords do not match.
     """
 
-    permission_classes = [IsAuthenticated]
+    serializer_class = user_serializers.SignUpSerializer
+
+
+class ProfileView(rest_generics.RetrieveUpdateAPIView):
+    """
+    API view for retrieving and updating the authenticated user's profile.
+
+    This view allows authenticated users to fetch their profile details
+    and update allowed profile fields using partial updates.
+
+    Authentication: JWTAuthentication:
+        Requires a valid JWT access token.
+
+    Permissions: IsAuthenticated:
+        Only authenticated users can access this endpoint.
+
+    Methods Allowed:
+        GET:
+            Retrieves the authenticated user's profile details.
+                Response:
+                    200 OK:
+                        - name (str | null)
+                        - email (str)
+                        - phone_number (str | null)
+                        - profile_picture (Image | null): User's profile image url.
+                        - city (City | null): User's city reference id.
+        PATCH
+            Update specific profile fields.
+                Request Body:
+                    - name: User's full name.,
+                    - profile_picture: User's profile picture.
+                    - phone_number: User's phone number.
+                    - city: User's city id.
+                Response:
+                    200 OK:
+                        - name (str | None)
+                        - email (str)
+                        - phone_number (str | None):
+                        - profile_picture (Image | None): User's profile image url.
+                        - city (City | None): User's city reference id.
+                Error:
+                    401 Unauthorized:
+                        - Token is expired
+                        - Authentication credentials were not provided
+                    400 Bad Request:
+                        - Phone number should be exactly 10 numeric digits
+    """
+
+    serializer_class = user_serializers.UserSerializer
+    permission_classes = [rest_permissions.IsAuthenticated]
     http_method_names = ["get", "patch"]
-
-    def get_serializer_class(self):
-        """
-        Return serializer based on HTTP method.
-        """
-        if self.request.method == "PATCH":
-            return ProfileUpdateSerializer
-        return UserProfileSerializer
 
     def get_object(self):
         """
-        Return the currently authenticated user.
+        Overrides the default behavior to return the currently logged-in user.
         """
         return self.request.user
-
-
-class BookingViewSet(ListModelMixin, UpdateModelMixin, GenericViewSet):
-    """
-    ViewSet to handle:
-    - Listing bookings of authenticated user.
-    - Cancelling a specific booking
-    """
-
-    permission_classes = [IsOwnerOrReadOnly]
-    http_method_names = ["get", "patch"]
-
-    def get_serializer_class(self):
-        if self.action == "partial_update":
-            return BookingCancelSerializer
-        return BookingHistorySerializer
-
-    def get_queryset(self):
-        if self.action == "list":
-            return (
-                Booking.objects.filter(user=self.request.user)
-                .prefetch_related("tickets_by_booking")
-                .select_related(
-                    "slot",
-                    "slot__cinema",
-                    "slot__cinema__city",
-                    "slot__movie",
-                )
-            )
-        return Booking.objects.filter().select_related("slot")
